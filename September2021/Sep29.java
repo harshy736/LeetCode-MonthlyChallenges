82a1b8c5d24df0a785d05bb48cc958d285c87863
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-09-29 21:35:29 +0530

    Create Sep29.java

---
+++ September2021/Sep29.java
@@ -0,0 +1,89 @@
+//Split Linked List in Parts
+
+/*
+Given the head of a singly linked list and an integer k, split the linked list into k consecutive linked list parts.
+
+The length of each part should be as equal as possible: no two parts should have a size differing by more than one. This may lead to some parts being null.
+
+The parts should be in the order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal to parts occurring later.
+
+Return an array of the k parts.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3], k = 5
+Output: [[1],[2],[3],[],[]]
+Explanation:
+The first element output[0] has output[0].val = 1, output[0].next = null.
+The last element output[4] is null, but its string representation as a ListNode is [].
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6,7,8,9,10], k = 3
+Output: [[1,2,3,4],[5,6,7],[8,9,10]]
+Explanation:
+The input has been split into consecutive parts with size difference at most 1, and earlier parts are a larger size than the later parts.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 1000].
+0 <= Node.val <= 1000
+1 <= k <= 50
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        if(head == null) return new ListNode[k];
+        
+        ListNode[] ans = new ListNode[k];
+        int size = 0;
+        ListNode tmp = head;
+        
+        //calculate size
+        while(tmp != null){
+            tmp = tmp.next;
+            size++;
+        }
+        
+        int listLen = size / k;//size of every list
+        int rem = size % k;//lists having 1 size extra
+         
+        tmp = head;
+        for(int i = 0; i < k; i++){
+            ListNode nhead = tmp;//first node
+            
+            for(int j = 1; j < listLen; j++){//adding sub nodes
+                tmp = tmp.next;
+            }
+            
+            if(i < rem && listLen > 0){//1 extra size
+                tmp = tmp.next;
+            }
+            
+            //break link
+            if(tmp != null){
+                ListNode forw = tmp.next;
+                tmp.next = null;
+                tmp = forw;
+            }
+            
+            ans[i] = nhead;
+        }
+        
+        return ans;
+    }
+}

75793b7b3bf785b880d2d9a1f015612125183762
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-11-27 23:59:45 +0530

    Update Nov28.java

--- November2021/Nov28.java
+++ November2021/Nov28.java
@@ -58,3 +58,29 @@ class Solution {
         return ans;
     }
 }
+
+/*2nd Sol -> O(1) extra space*/
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        
+        //O(1) extra space
+        int[] ans = new int[n];
+        
+        //first filling every pos with prefix product
+        ans[0] = 1;
+        for(int i = 1; i < n; i++)
+            ans[i] = ans[i - 1] * nums[i - 1];
+        
+        //now multiply prefix wrt suffix -> desired product
+        int suffix = 1;
+        for(int i = n - 1; i >= 0; i--){
+            ans[i] = ans[i] * suffix;//preix * suffix
+            suffix *= nums[i];
+        }
+        
+        return ans;
+    }
+}
+

2be4e17fee7864f641b18283cc516a127d6ae6d3
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-12-15 18:56:24 +0530

    Create Dec15.java

---
+++ December2021/Dec15.java
@@ -0,0 +1,58 @@
+/*
+147. Insertion Sort List
+
+The following is a graphical example of the insertion sort algorithm. The partially sorted list (black) initially contains only the first element in the list. One element (red) is removed from the input data and inserted in-place into the sorted list with each iteration.
+
+
+ 
+
+Example 1:
+
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 5000].
+-5000 <= Node.val <= 5000
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode sort = insertionSortList(head.next);
+        
+        //perform insertion sort of head node
+        int cval = head.val;
+        ListNode curr = sort, prev = head;
+        while(curr != null && curr.val < cval){
+            prev.val = curr.val;
+            
+            prev = curr; 
+            curr = curr.next;
+        }
+        
+        //fill
+        prev.val = cval;
+        
+        return head;
+    }
+}

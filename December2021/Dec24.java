bf16334d61b6f83036b2686e3d2ab9d04a7ea19d
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2021-12-25 00:29:13 +0530

    Create Dec24.java

---
+++ December2021/Dec24.java
@@ -0,0 +1,44 @@
+/*
+56. Merge Intervals
+
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ 
+
+Constraints:
+
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+*/
+
+class Solution {
+    
+    public int[][] merge(int[][] intervals) {
+       Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        
+        LinkedList<int[]> list = new LinkedList<>();
+        list.addLast(intervals[0]);
+        
+        for(int i = 1;i<intervals.length;i++){
+            if(list.getLast()[1] >= intervals[i][0])
+                list.getLast()[1] = Math.max(list.getLast()[1],intervals[i][1]);
+            else
+                list.addLast(intervals[i]);
+            
+        }
+        return list.toArray(new int[list.size()][]);
+    }
+}

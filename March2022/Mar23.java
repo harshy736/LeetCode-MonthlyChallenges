5984b5934d10572bff9c1d4ac44e975c10bc0e05
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2022-03-23 16:18:55 +0530

    Create Mar23.java

---
+++ March2022/Mar23.java
@@ -0,0 +1,53 @@
+/*
+991. Broken Calculator
+Medium
+
+There is a broken calculator that has the integer startValue on its display initially. In one operation, you can:
+
+multiply the number on display by 2, or
+subtract 1 from the number on display.
+Given two integers startValue and target, return the minimum number of operations needed to display target on the calculator.
+
+Example 1:
+
+Input: startValue = 2, target = 3
+Output: 2
+Explanation: Use double operation and then decrement operation {2 -> 4 -> 3}.
+Example 2:
+
+Input: startValue = 5, target = 8
+Output: 2
+Explanation: Use decrement and then double {5 -> 4 -> 8}.
+Example 3:
+
+Input: startValue = 3, target = 10
+Output: 3
+Explanation: Use double, decrement and double {3 -> 6 -> 5 -> 10}.
+ 
+Constraints:
+
+1 <= x, y <= 109
+*/
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        if(startValue == target)
+            return 0;
+        else if(startValue > target)
+            return startValue - target;//only option to convert by subtracion
+        
+        //if startValue < target
+        
+        int half = (target + 1) / 2;
+        int op = brokenCalc(startValue, half);//operations in getting half
+        
+        op++;//for multiplication
+        //e.g 3 -> 6
+        
+        //eg. tar = 5, half = 3 , mul - 3 * 2 = 6
+        //6 - 1 = 5 -> additional subtraction
+        if(target % 2 == 1)
+            op++;//for one additional subtraction
+        
+        return op;
+    }
+}

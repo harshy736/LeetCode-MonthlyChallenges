36800c46b75eba95463fd44f7c4c5b91b345caf6
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2022-06-14 10:45:22 +0530

    Update Jun15.java

--- Jun2022/Jun15.java
+++ Jun2022/Jun15.java
@@ -1,6 +1,13 @@
-export class Todo{
-    sno!: number
-    title!: string
-    desc!: string
-    active! : boolean
-}
+<div class="container">
+    <h1 class="text-center mb-5">Todo List By Harsh</h1>
+<app-add-todo (todoAdd)="addTodo($event)"></app-add-todo>
+<div class="my-4">
+    <h4>Your Todos</h4>
+    <div *ngIf="this.todos.length===0; else elseBlock">No Todos to display</div>
+    <ng-template #elseBlock>
+        <div *ngFor = "let todo of todos; index as i">
+            <app-todo-item [todo] = "todo" [i]="i" (todoDelete)="deleteTodo($event)" (todoCheckBox)="toggleTodo($event)"></app-todo-item>
+        </div> 
+    </ng-template>
+</div>
+</div>

bfd5e09c13e6f004a9af62ce3a814b3169452005
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2022-06-14 10:48:50 +0530

    Update Jun15.java

--- Jun2022/Jun15.java
+++ Jun2022/Jun15.java
@@ -1,46 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { Todo } from "../../Todo";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './MyComponenets/about/about.component';
+import { TodosComponent } from './MyComponenets/todos/todos.component';
 
-@Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+const routes: Routes = [
+  {path:'', component:TodosComponent},
+  {path:'about', component:AboutComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class TodosComponent implements OnInit {
-  todos : Todo[];
-  localItem;
-
-  constructor() {
-    this.localItem = localStorage.getItem("todos");
-    if(this.localItem==null){
-      this.todos = []
-    }else{
-      this.todos = JSON.parse(this.localItem);
-    }
-   }
-
-  ngOnInit(): void {
-  }
-
-
-  deleteTodo(todo : Todo){
-    console.log(todo);
-    const index = this.todos.indexOf(todo);
-    this.todos.splice(index, 1);
-    localStorage.setItem("todos", JSON.stringify(this.todos));
-  }
-
-  addTodo(todo : Todo){
-    console.log(todo);
-    this.todos.push(todo);
-    localStorage.setItem("todos", JSON.stringify(this.todos));
-  }
-
-  toggleTodo(todo : Todo){
-    console.log(todo);
-    const index = this.todos.indexOf(todo);
-    this.todos[index].active = !this.todos[index].active;
-    localStorage.setItem("todos", JSON.stringify(this.todos));
-  }
-}
+export class AppRoutingModule { }

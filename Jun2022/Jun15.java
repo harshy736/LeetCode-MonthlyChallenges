63954f5dbd4f0f63a620a128ae1452d245b230ac
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2022-06-14 10:53:46 +0530

    Update Jun15.java

--- Jun2022/Jun15.java
+++ Jun2022/Jun15.java
@@ -1,14 +1,30 @@
-<div class="my 3">
-    <h4>Add a Todo</h4>
-    <form (ngSubmit)="onSubmit()">
-        <div class="mb-3">
-            <label for="title" class="form">Todo Title</label>
-            <input type="text" [(ngModel)]="title" class="form-control" id="title" name="title" aria-describedby="emailHelp">
-        </div>
-        <div class="mb-3">
-            <label class="form" for="desc">Todo Description</label>
-            <input type="text" [(ngModel)]="desc" class="form-control" id="desc" name="desc">
-        </div>
-        <button type="submit" class="btn btn-sm btn-primary">Add Todo</button>
-    </form>    
-</div>
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css']
+})
+export class AddTodoComponent implements OnInit {
+  title!: string;
+  desc!: string;
+  @Output() todoAdd : EventEmitter<Todo> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+    const todo = {
+      sno:1,
+      title:this.title,
+      desc:this.desc,
+      active:true
+    }
+
+    this.todoAdd.emit(todo);
+  }
+
+}

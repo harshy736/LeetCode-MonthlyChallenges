9cab8257b2b60484694639276ae91040a42e03a9
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2022-06-14 11:17:27 +0530

    Update Jun15.java

--- Jun2022/Jun15.java
+++ Jun2022/Jun15.java
@@ -1,4 +1,46 @@
-<div class="container my-4 col-md-8 offset-md-2 text-warning bg-secondary">
-    <h1>About this Angular App</h1>
-    <p>Angular is a platform and framework for building single-page client applications using HTML and TypeScript. Angular is written in TypeScript. It implements core and optional functionality as a set of TypeScript libraries that you import into your applications.</p>
-</div>
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Todo } from "../../Todo";
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos : Todo[];
+  localItem;
+
+  constructor() {
+    this.localItem = localStorage.getItem("todos");
+    if(this.localItem==null){
+      this.todos = []
+    }else{
+      this.todos = JSON.parse(this.localItem);
+    }
+   }
+
+  ngOnInit(): void {
+  }
+
+
+  deleteTodo(todo : Todo){
+    console.log(todo);
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+
+  addTodo(todo : Todo){
+    console.log(todo);
+    this.todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+
+  toggleTodo(todo : Todo){
+    console.log(todo);
+    const index = this.todos.indexOf(todo);
+    this.todos[index].active = !this.todos[index].active;
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+}

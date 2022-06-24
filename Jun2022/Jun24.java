9b67bfdf6b8ad8db6042da946fe43b78b328a993
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2022-06-24 15:55:36 +0530

    Create Jun24.java

---
+++ Jun2022/Jun24.java
@@ -0,0 +1,73 @@
+/*
+1354. Construct Target Array With Multiple Sums
+Hard
+
+You are given an array target of n integers. From a starting array arr consisting of n 1's, you may perform the following procedure :
+
+let x be the sum of all elements currently in your array.
+choose index i, such that 0 <= i < n and set the value of arr at index i to x.
+You may repeat this procedure as many times as needed.
+Return true if it is possible to construct the target array from arr, otherwise, return false.
+
+Example 1:
+
+Input: target = [9,3,5]
+Output: true
+Explanation: Start with arr = [1, 1, 1] 
+[1, 1, 1], sum = 3 choose index 1
+[1, 3, 1], sum = 5 choose index 2
+[1, 3, 5], sum = 9 choose index 0
+[9, 3, 5] Done
+Example 2:
+
+Input: target = [1,1,1,2]
+Output: false
+Explanation: Impossible to create target array from [1,1,1,1].
+Example 3:
+
+Input: target = [8,5]
+Output: true
+ 
+
+Constraints:
+
+n == target.length
+1 <= n <= 5 * 104
+1 <= target[i] <= 109
+*/
+class Solution {
+    public boolean isPossible(int[] target) {//Hint
+        long sum = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int tar : target){
+            sum += tar;
+            pq.offer(tar);
+        }
+        
+        //every largest element is sum of the whole sum array from previous step
+        //try to find previous value of this largest element
+        //Solve in reverse order
+        //try to convert target to intial array
+        //if any element becomes less than 1 -> ot possible
+        
+        
+        //until array becomes intial
+        while(pq.peek() != 1){
+            int max = pq.poll();
+            sum -= max;
+            
+            if(sum == 1)
+                return true;
+            
+            if(max < sum || sum == 0 || max % sum == 0)//not possible
+                return false;
+            
+            max %= sum;
+            sum += max;
+            pq.offer(max);
+        }
+        
+        return true;
+    }
+}

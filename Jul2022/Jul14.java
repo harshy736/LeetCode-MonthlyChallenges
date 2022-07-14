16c66ba5680943f0add492f595207b3a326762a4
Harsh yadav <68581921+harshy736@users.noreply.github.com>
2022-07-14 10:44:26 +0530

    Create Jul14.java

---
+++ Jul2022/Jul14.java
@@ -0,0 +1,75 @@
+/*
+105. Construct Binary Tree from Preorder and Inorder Traversal
+Medium
+
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+Example 1:
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+Example 2:
+
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+ 
+Constraints:
+
+1 <= preorder.length <= 3000
+inorder.length == preorder.length
+-3000 <= preorder[i], inorder[i] <= 3000
+preorder and inorder consist of unique values.
+Each value of inorder also appears in preorder.
+preorder is guaranteed to be the preorder traversal of the tree.
+inorder is guaranteed to be the inorder traversal of the tree.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        return buildTreeHelper(preorder, 0, n - 1, inorder, 0, n - 1);
+    }
+    
+    private TreeNode buildTreeHelper(int[] preorder, int ps, int pe, int[] inorder, int is, int ie){
+        if(ps > pe)return null; //no node exists
+        if(ps == pe)//leaf node
+            return new TreeNode(preorder[ps]);
+        
+        //first node of preorder is root node
+        TreeNode root = new TreeNode(preorder[ps]);
+        
+        //In inoredr all the node left to root is left subtree
+        //and right to root is right subtree
+        
+        int left = 0;//nodes in left subtree
+        //search root
+        for(int i = is; i <= ie; i++){
+            if(inorder[i] == root.val)
+                break;
+            
+            left++;
+        }
+        
+        //now get left subtree
+        root.left = buildTreeHelper(preorder, ps + 1, ps + left, inorder, is, is + left - 1);
+        
+        //now get right subtree
+        root.right = buildTreeHelper(preorder, ps + left + 1, pe, inorder, is + left + 1, ie);
+        
+        return root;
+    }
+}
